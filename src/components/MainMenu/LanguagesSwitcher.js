57116521d8b6668e4a2b5d0c3bbf8c36e3debaa6
Eugeneche <eugene.bizmail@gmail.com>
2023-05-19 21:15:31 +0200

    useEffect instead isBrowser

--- src/components/MainMenu/LanguagesSwitcher.js
+++ src/components/MainMenu/LanguagesSwitcher.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 import * as style from "./_MainMenu.module.scss"
 import { LocaleContext } from "../layout"
@@ -13,32 +13,45 @@ import cze from "../../images/cze.svg"
 
 const LanguagesSwitcher = () => {
 
-  const isBrowser = typeof window !== "undefined"
+  //const isBrowser = typeof window !== "undefined"
   //const { headerHero } = useTranslations()
+
+  const [path, setPath] = useState('')
 
   const locale = React.useContext(LocaleContext)
 
-  if (!isBrowser) {
+  /* if (!isBrowser) {
     return
-  }
+  } */
 
-  const path = () => {
+/*   const path = () => {
     if (window.location.pathname.match(`/${locale.locale}/`)) {
       return window.location.pathname.slice(3)
     } else { 
       return window.location.pathname
     }
-  }
+  } */
 
+  useEffect(() => {
+    if (window.location.pathname.match(`/${locale.locale}/`)) {
+      setPath(window.location.pathname.slice(3))
+    } else { 
+      setPath(window.location.pathname)
+    }
+
+    console.log(window.location)
+  })
+
+  
   return (
     <nav>
-      <Link to={`${path()}`} hrefLang="en">
+      <Link to={`${path}`} hrefLang="en">
         <img className={style.flag} src={eng} alt="great britain flag"></img>
       </Link>
-      <Link to={`/cs${path()}`} hrefLang="cs">
+      <Link to={`/cs${path}`} hrefLang="cs">
         <img className={style.flag} src={cze} alt="czech flag"></img>
       </Link>
-      <Link to={`/uk${path()}`} hrefLang="uk">
+      <Link to={`/uk${path}`} hrefLang="uk">
         <img className={style.flag} src={ukr} alt="ukrainian flag"></img>
       </Link>
 {/*       {` `}/{` `}
