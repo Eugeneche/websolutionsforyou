1ac8bdb2c9a17360a8af6722b6274e7d94df832e
Eugeneche <eugene.bizmail@gmail.com>
2023-05-19 18:20:33 +0200

    Check for errors w/o Header

--- src/utils/gatsby-node-helpers.js
+++ src/utils/gatsby-node-helpers.js
@@ -1,12 +1,13 @@
 // Use a little helper function to remove trailing slashes from paths
-export function removeTrailingSlash(path)
-  {   return path === `/` ? path : path.replace(/\/$/, ``)}
+exports.removeTrailingSlash = path =>
+  path === `/` ? path : path.replace(/\/$/, ``)
 
-export function localizedSlug({ isDefault, locale, slug })  {   return isDefault ? `/${slug}` : `/${locale}/${slug}`}
+exports.localizedSlug = ({ isDefault, locale, slug }) =>
+  isDefault ? `/${slug}` : `/${locale}/${slug}`
 
 // From lodash:
 // https://github.com/lodash/lodash/blob/750067f42d3aa5f927604ece2c6df0ff2b2e9d72/findKey.js
-export function findKey(object, predicate) {
+exports.findKey = (object, predicate) => {
   let result
   if (object == null) {
     return result
