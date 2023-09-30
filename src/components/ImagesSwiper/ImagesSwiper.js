7f35b1c0c94ddb7d6574228ea0ac393d0e9fb0f8
Eugeneche <eugene.bizmail@gmail.com>
2023-09-30 15:00:46 +0200

    Added gallery for multiply view

---
+++ src/components/ImagesSwiper/ImagesSwiper.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { GatsbyImage } from "gatsby-plugin-image"
+import * as styles from "./_ImagesSwiper.module.scss"
+//import "./imagesSwiper.scss"
+import Carousel from "nuka-carousel"
+
+import left from "../../images/arrow-chevron-left.svg"
+import right from "../../images/arrow-chevron-right.svg"
+
+
+const ImagesSwiper = (props) => {
+
+    return (
+        <Carousel 
+            wrapAround={true}
+            pauseOnHover={false}
+            slideIndex={props.currentSlide}
+            /* className={styles.gallery} */
+/*             defaultControlsConfig={{
+                nextButtonStyle: {display: "none"},
+                prevButtonStyle: {display: "none"},
+                pagingDotsClassName: "dot",
+                pagingDotsContainerClassName: "dots"
+            }} */
+            renderCenterLeftControls={({ previousSlide }) => (
+                <button onClick={previousSlide}><img className={styles.arrowLeft} src={left}></img></button>
+              )}
+              renderCenterRightControls={({ nextSlide }) => (
+                <button onClick={nextSlide}><img className={styles.arrowRight} src={right}></img></button>
+              )}
+        >
+            {props.images.map(img => {
+
+                return (
+                    <div className={styles.slide} key={img.childImageSharp.id}>
+                        
+                        <GatsbyImage 
+                            className={styles.increased}
+                            image={img.childImageSharp.gatsbyImageData} 
+                            alt={`Image of`} 
+                            objectPosition="50% 50%"
+                        />
+
+                    </div>
+                )
+            })}
+          
+        </Carousel>
+    )
+}
+
+export default ImagesSwiper
--- src/templates/project.js
+++ src/templates/project.js
@@ -1,36 +1,75 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 import Seo from "../components/seo"
 import * as styles from "../style/_style.module.scss"
 import { GatsbyImage } from "gatsby-plugin-image"
+import close from "../images/close.svg"
+import ImagesSwiper from "../components/ImagesSwiper/ImagesSwiper"
 
-// The normal <a> tag is modified here (so that internal links use gatsby-link/LocalizedLink
-// More info:
-// https://www.gatsbyjs.com/docs/mdx/customizing-components/
+
 const Project = ({data, pageContext, children}) => {
-  
-  //console.log(pageContext)
 
-  //const images = data.allFile.nodes
+  const [ showImageFull, setShowImageFull ] = useState(false)
+  const [ currentImageIndex, setCurrentImageIndex ] = useState(0)
+
   const imageCover = data.allFile.nodes.filter(node => node.name === `cover`)
   const imageAll = data.allFile.nodes.filter(node => node.name === `all`)
-  //console.log(data)
+  const imagesNumber = data.allFile.nodes.length
+  const imagesWithoutCover = data.allFile.nodes.filter(node => node.name !== `cover`).sort((a, b) => a.name - b.name)
+  const imageTitles = data?.mdx?.frontmatter?.img_titles
+  
+  //console.log(imageTitles)
+  const showFullImage = (i) => {
+    setCurrentImageIndex(i)
+    setShowImageFull(true)
+  }
   return (
     <>
+      {showImageFull && <div className={styles.fullImage}>
+        <button onClick={() => setShowImageFull(false)}><img className={styles.close} src={close}></img></button>
+        <ImagesSwiper images={imagesWithoutCover} currentSlide={currentImageIndex} />
+      </div>}
+
       <div className={styles.container}> 
         <h1  className={styles.projectsHeader}>{data.mdx.frontmatter.title}</h1>
-        <div className={styles.projectsImages}>
-          <GatsbyImage 
-            image={imageCover[0].childImageSharp.gatsbyImageData}
-            className={styles.projectImage}
-            alt={`Website for ${data.mdx.frontmatter.title} general view`}
-          />
-          <GatsbyImage 
-            image={imageAll[0].childImageSharp.gatsbyImageData}
-            className={styles.projectImage}
-            alt={`Website for ${data.mdx.frontmatter.title} view in devices`}
-          />
-        </div>
+        {imagesNumber <= 2 ?
+          <div className={styles.projectsTwoImages}>
+            <GatsbyImage 
+              image={imageCover[0]?.childImageSharp?.gatsbyImageData}
+              className={styles.projectImage}
+              alt={`Website for ${data.mdx.frontmatter.title} general view`}
+            />
+            <GatsbyImage 
+              image={imageAll[0]?.childImageSharp?.gatsbyImageData}
+              className={styles.projectImage}
+              alt={`Website for ${data.mdx.frontmatter.title} view in different devices`}
+            />
+          </div> :
+
+          <div className={styles.projectsGalleryImages}>
+            <GatsbyImage 
+              image={imageCover[0].childImageSharp.gatsbyImageData}
+              className={styles.projectImage}
+              alt={`${data.mdx.frontmatter.title} general view`}
+            />
+            <div className={styles.gallery}>
+              {imagesWithoutCover.map((img, i) => {
+
+                return (
+                  <div className={styles.galleryItemContainer} onClick={() => showFullImage(i)} key={img.childImageSharp.id}>
+                    <h4 className={styles.projectImageTitle}>{imageTitles[i]}</h4>
+                    <GatsbyImage                      
+                      image={img.childImageSharp.gatsbyImageData}
+                      className={styles.projectImageGallery}
+                      alt={`Page of ${data.mdx.frontmatter.title}`}
+                    />
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        }
+        
         <div className={styles.contentFromMdx} dangerouslySetInnerHTML={{__html: `${data.mdx.body}`}} />
       </div>
     </>
@@ -64,6 +103,7 @@ export const query = graphql`
         seo_description
         seo_title
         title
+        img_titles
       }
     }
   }
