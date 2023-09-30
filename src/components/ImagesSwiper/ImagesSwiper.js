2864884120aca3d61b7c03833b38388ae9a835ee
Eugeneche <eugene.bizmail@gmail.com>
2023-10-01 00:32:24 +0200

    5

--- src/components/ImagesSwiper/ImagesSwiper.js
+++ src/components/ImagesSwiper/ImagesSwiper.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { GatsbyImage } from "gatsby-plugin-image"
 import * as styles from "./_ImagesSwiper.module.scss"
-import "./imagesSwiper.scss"
+//import "./imagesSwiper.scss"
 import Carousel from "nuka-carousel"
 
 import left from "../../images/arrow-chevron-left.svg"
