1ffcbafbd092ec0b6e183c4c61e602307b722452
Eugeneche <eugene.bizmail@gmail.com>
2024-07-30 19:07:21 +0200

    Google search console code

--- src/html.js
+++ src/html.js
@@ -11,27 +11,8 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <meta name="google-site-verification" content="UMTW0Nvb5HI4rn43tZ-XNZtz1Pjkc1lKfpA67YdbeQ4"></meta>
         {props.headComponents}
-        
-        {/* <script type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-         
-            ym(94176286, "init", {
-                 clickmap:true,
-                 trackLinks:true,
-                 accurateTrackBounce:true,
-                 webvisor:true
-            });
-            `,
-          }}
-        />
-        <noscript><div><img src="https://mc.yandex.ru/watch/94176286" style={{position:"absolute", left:"-9999px"}} alt="" /></div></noscript> */}
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
