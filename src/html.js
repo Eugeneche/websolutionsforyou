63f916b13ed49ac3b88c6e634327057fa2709349
Yevhen <53338476+Eugeneche@users.noreply.github.com>
2024-08-07 06:11:54 +0200

    Update html.js

--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <meta name="google-site-verification" content="UMTW0Nvb5HI4rn43tZ-XNZtz1Pjkc1lKfpA67YdbeQ4"></meta>
+        <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/47c01180352bbefde18454c0/script.js"></script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
